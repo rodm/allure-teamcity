081503b3fd03ae702632aad30e6203ef05d31b74
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2023-06-13 16:57:12 +0100

    Clean up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,9 @@
-import net.researchgate.release.ReleaseExtension
 
 val linkHomepage by extra("https://qameta.io/allure")
 val linkCi by extra("https://ci.qameta.io/job/allure-teamcity")
 val linkScmUrl by extra("https://github.com/allure-framework/allure-teamcity")
 val linkScmConnection by extra("scm:git:git://github.com/allure-framework/allure-teamcity.git")
 val linkScmDevConnection by extra("scm:git:ssh://git@github.com:allure-framework/allure-teamcity.git")
-
-tasks.wrapper {
-    gradleVersion = "7.5.1"
-}
 
 plugins {
     id("org.owasp.dependencycheck") version "7.4.4"
@@ -18,18 +13,14 @@ plugins {
 description = "Allure Teamcity"
 group = "io.qameta.allure"
 
-configurations.all {
-    resolutionStrategy.eachDependency {
-        when (requested.module.toString()) {
-            "xml-apis:xml-apis" -> useVersion("1.4.01")
-        }
-    }
-}
-
-configure<ReleaseExtension> {
+release {
     tagTemplate.set("$version")
 }
 
 tasks.afterReleaseBuild {
     dependsOn(":allure-teamcity-server:publishPlugin")
+}
+
+tasks.wrapper {
+    gradleVersion = "7.5.1"
 }
